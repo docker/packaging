9b73a51b28dcbaa7d65116704eb985e536a280c4
CrazyMax <crazy-max@users.noreply.github.com>
2022-09-05 23:35:39 +0200

    docker-cli: internal folder for scripts and config
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

---
+++ pkg/docker-cli/internal/pkg-deb-build.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Docker Packaging authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+: "${DOCKER_CLI_VERSION=}"
+
+: "${PKG_NAME=}"
+: "${PKG_RELEASE=}"
+: "${PKG_DISTRO=}"
+: "${PKG_SUITE=}"
+: "${PKG_PACKAGER=}"
+: "${PKG_VENDOR=}"
+
+: "${PKG_DEB_REVISION=}"
+: "${PKG_DEB_EPOCH=}"
+
+: "${SOURCE_DATE_EPOCH=}"
+: "${SRCDIR=/work/src}"
+: "${OUTDIR=/out}"
+
+set -e
+
+if [ -z "$PKG_RELEASE" ]; then
+  echo >&2 "error: PKG_RELEASE is required"
+  exit 1
+fi
+if [ -z "$OUTDIR" ]; then
+  echo >&2 "error: OUTDIR is required"
+  exit 1
+fi
+
+mkdir -p "$OUTDIR"
+
+if ! command -v xx-info &> /dev/null; then
+  echo >&2 "error: xx cross compilation helper is required"
+  exit 1
+fi
+
+# TODO: add support for cross comp
+if xx-info is-cross; then
+  echo >&2 "warning: cross compilation with $(xx-info arch) not supported"
+  exit 0
+fi
+
+tilde='~'
+debVersion="${DOCKER_CLI_VERSION#v}"
+debVersion="${debVersion//-/$tilde}"
+
+cat > "debian/changelog" <<-EOF
+${PKG_NAME} (${PKG_DEB_EPOCH}$([ -n "$PKG_DEB_EPOCH" ] && echo ":")${debVersion}-${PKG_DEB_REVISION}) $PKG_SUITE; urgency=low
+  * Version: $DOCKER_CLI_VERSION
+ -- $(awk -F ': ' '$1 == "Maintainer" { print $2; exit }' debian/control)  $(date --rfc-2822)
+EOF
+
+if [ -d "${SRCDIR}" ]; then
+  commit="$(git --git-dir ${SRCDIR}/.git rev-parse HEAD)"
+fi
+
+set -x
+
+chmod -x debian/compat debian/control debian/docs debian/*.bash-completion debian/*.manpages
+DOCKER_CLI_REVISION=$commit dpkg-buildpackage -us -uc
+
+pkgoutput="${OUTDIR}/${PKG_DISTRO}/${PKG_SUITE}/$(xx-info arch)"
+if [ -n "$(xx-info variant)" ]; then
+  pkgoutput="${pkgoutput}/$(xx-info variant)"
+fi
+mkdir -p "${pkgoutput}"
+cp /root/docker-* "${pkgoutput}"/
---
+++ pkg/docker-cli/internal/pkg-static-build.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Docker Packaging authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+: "${DOCKER_CLI_VERSION=}"
+
+: "${BUILDDIR=/work/build}"
+: "${SRCDIR=/work/src}"
+: "${OUTDIR=/out}"
+
+set -e
+
+if [ -z "$OUTDIR" ]; then
+  echo >&2 "error: OUTDIR is required"
+  exit 1
+fi
+
+mkdir -p "$OUTDIR"
+
+if ! command -v xx-info &> /dev/null; then
+  echo >&2 "error: xx cross compilation helper is required"
+  exit 1
+fi
+
+# TODO: add support for cross comp
+if xx-info is-cross; then
+  echo >&2 "warning: cross compilation with $(xx-info arch) not supported"
+  exit 0
+fi
+
+pkgoutput="/out/static/$(xx-info os)/$(xx-info arch)"
+if [ -n "$(xx-info variant)" ]; then
+  pkgoutput="${pkgoutput}/$(xx-info variant)"
+fi
+
+mkdir -p "${pkgoutput}"
+
+cd "$BUILDDIR"
+for pkgname in *; do
+  workdir=$(mktemp -d -t docker-packaging.XXXXXXXXXX)
+  mkdir -p "$workdir/${pkgname}"
+  (
+    set -x
+    cp "${pkgname}"/* ${SRCDIR}/LICENSE ${SRCDIR}/README.md "$workdir/${pkgname}/"
+  )
+  if [ "$(xx-info os)" = "windows" ]; then
+    (
+      set -x
+      cd "$workdir"
+      zip -r "${pkgoutput}/${pkgname}_${DOCKER_CLI_VERSION#v}.zip" "${pkgname}"
+    )
+  else
+    (
+      set -x
+      tar -czf "${pkgoutput}/${pkgname}_${DOCKER_CLI_VERSION#v}.tgz" -C "$workdir" "${pkgname}"
+    )
+  fi
+done
