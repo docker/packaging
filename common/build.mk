a4e59898f8149f3b2431dea886ac9cc171b4f512
CrazyMax <crazy-max@users.noreply.github.com>
2022-09-24 19:03:50 +0200

    common: handle empty platform case for some pkgs
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- common/build.mk
+++ common/build.mk
@@ -14,14 +14,18 @@
 
 define run_bake
 	$(eval $@_TMP_OUT := $(shell mktemp -d -t docker-packaging.XXXXXXXXXX))
-	$(eval FILTERED_PLATFORMS = $(filter $(PKG_SUPPORTED_PLATFORMS),$(4)))
-	$(eval PLATFORMS = $(if $(PKG_SUPPORTED_PLATFORMS:-=),$(FILTERED_PLATFORMS),$(4)))
-	@PKG_RELEASE=$(1) DESTDIR=$(2) docker buildx bake $(foreach platform,$(PLATFORMS),--set "*.platform=$(platform)") $(3) pkg --print
-	PKG_RELEASE=$(1) DESTDIR=$($@_TMP_OUT) docker buildx bake $(foreach platform,$(PLATFORMS),--set "*.platform=$(platform)") $(3) pkg
+	@PKG_RELEASE=$(1) DESTDIR=$(2) docker buildx bake $(foreach platform,$(4),--set "*.platform=$(platform)") $(3) pkg --print
+	PKG_RELEASE=$(1) DESTDIR=$($@_TMP_OUT) docker buildx bake $(foreach platform,$(4),--set "*.platform=$(platform)") $(3) pkg
 	mkdir -p $(2)
 	find $($@_TMP_OUT) -mindepth 2 -maxdepth 2 -type d -exec cp -rf {} $(2)/ ';'
 	find $(2) -type d -empty -delete
 	rm -rf "$($@_TMP_OUT)"
+endef
+
+define build_pkg
+	$(eval FILTERED_PLATFORMS = $(filter $(PKG_SUPPORTED_PLATFORMS),$(4)))
+	$(eval PLATFORMS = $(if $(PKG_SUPPORTED_PLATFORMS:-=),$(FILTERED_PLATFORMS),$(4)))
+	$(if $(PLATFORMS:-=),$(call run_bake,$(1),$(2),$(3),$(PLATFORMS)),$(info no platform compatible for $(1)))
 endef
 
 .PHONY: pkg
@@ -50,8 +54,8 @@ pkg-multi-static:
 
 .PHONY: build-%
 build-%: pkg-info-%
-	$(call run_bake,$*,$(DESTDIR),$(BAKE_DEFINITIONS),)
+	$(call build_pkg,$*,$(DESTDIR),$(BAKE_DEFINITIONS),)
 
 .PHONY: build-multi-%
 build-multi-%: pkg-info-%
-	$(call run_bake,$*,$(DESTDIR),$(BAKE_DEFINITIONS),$(PKG_PLATFORMS))
+	$(call build_pkg,$*,$(DESTDIR),$(BAKE_DEFINITIONS),$(PKG_PLATFORMS))
