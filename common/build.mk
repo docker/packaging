85ea50d16114afc1f97704c15de572e085ce94fb
CrazyMax <crazy-max@users.noreply.github.com>
2022-09-30 17:09:40 +0200

    mutualize build and bake make functions
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- common/build.mk
+++ common/build.mk
@@ -12,31 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-define run_bake
+define bake
 	$(eval $@_TMP_OUT := $(shell mktemp -d -t docker-packaging.XXXXXXXXXX))
-	@PKG_RELEASE=$(1) PKG_TYPE=$(PKG_TYPE) DESTDIR=$(2) docker buildx bake $(foreach platform,$(4),--set "*.platform=$(platform)") $(3) pkg --print
-	PKG_RELEASE=$(1) PKG_TYPE=$(PKG_TYPE) DESTDIR=$($@_TMP_OUT) docker buildx bake $(foreach platform,$(4),--set "*.platform=$(platform)") $(3) pkg
+	@PKG_RELEASE=$(1) PKG_TYPE=$(PKG_TYPE) DESTDIR=$(2) docker buildx bake $(foreach platform,$(5),--set "*.platform=$(platform)") $(3) $(4) --print
+	PKG_RELEASE=$(1) PKG_TYPE=$(PKG_TYPE) DESTDIR=$($@_TMP_OUT) docker buildx bake $(foreach platform,$(5),--set "*.platform=$(platform)") $(3) $(4)
 	mkdir -p $(2)
 	find $($@_TMP_OUT) -mindepth 2 -maxdepth 2 -type d -exec cp -rf {} $(2)/ ';'
 	find $(2) -type d -empty -delete
 	rm -rf "$($@_TMP_OUT)"
 endef
 
-# build_pkg will filter PKG_SUPPORTED_PLATFORMS of the base pkg image with
-# projects ones in $(4) and call run_bake. If LIMITED_PLATFORMS is set, then it
+# build will filter PKG_SUPPORTED_PLATFORMS of the base pkg image with
+# projects ones in $(5) and call run_bake. If LIMITED_PLATFORMS is set, then it
 # will filter against those. If LOCAL_PLATFORM is set, then it will build only
 # for the current local platform (no platform passed). Static package is a
 # special case where PKG_SUPPORTED_PLATFORMS is always empty as this kind of
 # package is built with cross compilation support and therefore all supported
 # platforms of the project are always passed.
-define build_pkg
-	$(eval SUPPORTED_PLATFORMS = $(if $(LIMITED_PLATFORMS:-=),$(LIMITED_PLATFORMS),$(4)))
+define build
+	$(eval SUPPORTED_PLATFORMS = $(if $(LIMITED_PLATFORMS:-=),$(LIMITED_PLATFORMS),$(5)))
 	$(eval FILTERED_PLATFORMS = $(filter $(PKG_SUPPORTED_PLATFORMS),$(SUPPORTED_PLATFORMS)))
-	$(eval BUILD_PLATFORMS = $(if $(PKG_SUPPORTED_PLATFORMS:-=),$(FILTERED_PLATFORMS),$(4)))
+	$(eval BUILD_PLATFORMS = $(if $(PKG_SUPPORTED_PLATFORMS:-=),$(FILTERED_PLATFORMS),$(5)))
 	$(if $(LOCAL_PLATFORM:-=), \
-		$(call run_bake,$(1),$(2),$(3),), \
+		$(call bake,$(1),$(2),$(3),$(4),), \
 		$(if $(BUILD_PLATFORMS:-=), \
-			$(call run_bake,$(1),$(2),$(3),$(BUILD_PLATFORMS)), \
+			$(call bake,$(1),$(2),$(3),$(4),$(BUILD_PLATFORMS)), \
 			$(info $(SUPPORTED_PLATFORMS) platform(s) not supported by $(1)) \
 		) \
 	)
@@ -48,13 +48,21 @@ pkg:
 
 .PHONY: pkg-%
 pkg-%: pkg-%-releases
-	$(MAKE) $(foreach release,$(PKG_RELEASES),build-$(release))
+	$(MAKE) $(foreach release,$(PKG_RELEASES),run-pkg-$(release))
 
-.PHONY: build-%
-build-%: pkg-info-%
-	$(call build_pkg,$*,$(DESTDIR),$(BAKE_DEFINITIONS),$(PKG_PLATFORMS))
+.PHONY: run-pkg-%
+run-pkg-%: pkg-info-%
+	$(call build,$*,$(DESTDIR),$(BAKE_DEFINITIONS),pkg,$(PKG_PLATFORMS))
+
+.PHONY: verify
+verify:
+	$(MAKE) $(foreach pkg,$(PKG_LIST),verify-$(pkg))
 
 .PHONY: verify-%
-verify-%: pkg-info-%
-	@PKG_RELEASE=$* PKG_TYPE=$(PKG_TYPE) docker buildx bake $(BAKE_DEFINITIONS) verify --print
-	PKG_RELEASE=$* PKG_TYPE=$(PKG_TYPE) docker buildx bake $(BAKE_DEFINITIONS) verify
+verify-%: pkg-%-releases
+	$(MAKE) $(foreach release,$(PKG_RELEASES),run-verify-$(release))
+
+.PHONY: run-verify-%
+run-verify-%: pkg-info-%
+	$(eval LOCAL_PLATFORM = 1) # TODO: add support for cross platform verification
+	$(call build,$*,$(DESTDIR),$(BAKE_DEFINITIONS),verify,$(PKG_PLATFORMS))
