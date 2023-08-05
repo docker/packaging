f44da7ce7cb5e1a189bf5fe28cccb084bb9acff1
CrazyMax <crazy-max@users.noreply.github.com>
2023-08-05 11:17:29 +0200

    rpm: add almalinux 8 and 9
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- common/scripts/rpm-init.sh
+++ common/scripts/rpm-init.sh
@@ -83,11 +83,11 @@ case "$pkgrelease" in
   fedora*)
     dnf install -y git rpm-build rpmlint dnf-plugins-core
     ;;
-  rockylinux8)
+  rockylinux8|almalinux8)
     dnf install -y git rpm-build rpmlint dnf-plugins-core epel-release
-    dnf config-manager --set-enabled devel
+    dnf config-manager --set-enabled powertools
     ;;
-  rockylinux*)
+  rockylinux*|almalinux*)
     dnf install -y git rpm-build rpmlint dnf-plugins-core epel-release
     dnf config-manager --set-enabled crb
     ;;
