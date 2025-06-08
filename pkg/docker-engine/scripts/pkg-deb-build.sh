3b2d6493c0bafd2709dad04df60ac8b1ddc3ad42
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-06-08 17:39:13 +0200

    pkg(docker-engine): add /etc/docker directory
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- pkg/docker-engine/scripts/pkg-deb-build.sh
+++ pkg/docker-engine/scripts/pkg-deb-build.sh
@@ -66,6 +66,6 @@ mkdir -p "${pkgoutput}"
 
 set -x
 
-chmod -x debian/control debian/docs
+chmod -x debian/control debian/docs debian/docker-ce.dirs
 VERSION=${GENVER_VERSION} REVISION=${GENVER_COMMIT_SHORT} dpkg-buildpackage $PKG_DEB_BUILDFLAGS --host-arch $(xx-info debian-arch) --target-arch $(xx-info debian-arch)
 cp /root/docker-* "${pkgoutput}"/
